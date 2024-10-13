22f5b71ef8804e924e8e087449b5ab4d1eb320a6
Yugi26 <wat.tsuku26@gmail.com>
2024-10-13 13:12:35 +0900

    所属の操作の追加によるルート設定の更新

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Affiliation;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AffiliationController::class, 'showForm']);
+
+// 動的な選択肢を取得するためのルート
+Route::get('/department', [AffiliationController::class, 'getDepartment']);
+Route::get('/major', [AffiliationController::class, 'getMajor']);
