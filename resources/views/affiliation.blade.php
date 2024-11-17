197ad21ca5b14303050d42b7c9abbf34eb907e6d
Yugi26 <wat.tsuku26@gmail.com>
2024-11-17 23:45:48 +0900

    タイトルと見出しを「筑波大学大学院 卒業判定」に変更し、履修履歴CSVのダウンロード方法と注意事項を追加

--- resources/views/affiliation.blade.php
+++ resources/views/affiliation.blade.php
@@ -2,12 +2,12 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>学術院・研究群・学位プログラムの選択</title>
+    <title>筑波大学大学院 卒業判定</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <h1>学術院・研究群・学位プログラムの選択</h1>
+    <h1>筑波大学大学院 卒業判定</h1>
     <form action="/check" method="POST" enctype="multipart/form-data">
         @csrf
 
@@ -37,11 +37,22 @@
 
         <label for="csv_file">履修履歴CSV:</label>
         <input type="file" id="csv_file" name="csv_file" required>
-
-        <br><br>
+        <p>
+            履修履歴ファイルは
+            <a href="https://twins.tsukuba.ac.jp/campusweb/campusportal.do?page=main&tabId=si" target="_blank" rel="noopener noreferrer">
+                TWINS
+            </a>
+            の成績ページでダウンロードできます。形式はCSV、日本語（シフトJIS）にしてください。
+        </p>
 
         <button type="submit">卒業要件を確認</button>
+
+        <br><br>
     </form>
+
+    <footer>
+        <p>※履修履歴CSVはデータベースには保存されず、メモリ上で処理後に削除されます。</p>
+    </footer>
 
     <script>
         $(document).ready(function() {
